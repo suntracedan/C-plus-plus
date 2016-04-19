befe831d928a08d94081830247c963610256851e
suntracedan <13474153341@163.com>
2016-04-19 11:04:06 +0800

    03-01

---
+++ 03-01/03-01/test.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+
+
+int main()
+{
+	int i=0;
+	int arr[16]={0};
+	for(i=0;i<=18;i++)
+	{
+		arr[i]=0;
+		//printf("%d",i);
+		cout<<i;
+	}
+	system("pause");
+	return 0;
+}
+
+
+
